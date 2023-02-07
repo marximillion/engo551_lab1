7783ee5412efbb090aee34eb4069b85c5904b387
marximillion <marximillion@gmail.com>
2023-02-07 01:09:40 -0700

    New Feature Added: Login and Register | File cleanup

--- application.py
+++ application.py
@@ -1,12 +1,5 @@
 # postgresql://postgres:23081201@localhost/engo551_lab1
 # import requests
-import os
-import csv
-
-from flask import Flask, session, render_template, request
-from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 from models import *
 #from flask_sqlalchemy import SQLAlchemy
 
@@ -20,14 +13,19 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
+bootstrap = Bootstrap(app)
+
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 db.init_app(app)
 Session(app)
-
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 
 @app.route("/")
@@ -37,102 +35,65 @@ def main():
     reader = csv.reader(f)
     for isbn, name, author, year in reader:
         book = Books(isbn=isbn, name=name, author=author,
-                        year=year)
+                     year=year)
     db.session.add(book)
     db.session.commit()
-    return render_template("index.html") 
+    return render_template("index.html")
 
-@app.route("/register")
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    form = RegisterForm()
+    username = form.username.data
+    password = form.password.data
+    message = "Registration"
 
-#@app.route("/login")
-#def register():
-#	if session.get("username") is not None:
-#		return render_template("home.html", username = session.get("username"))
-#	return render_template("login.html")
+    if form.validate_on_submit():
+        new_user = Users(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
 
-@app.route("/success", methods=["POST"])
-def success():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    user = Users(username=username, password=password)
-    #user_exists = Users.query(username)
-    #if user_exists == username:
-    #    print(f"username: {username}")
-    #    return render_template("error.html")
-    db.session.add(user)
-    db.session.commit()
-    return render_template("success.html", username=user.username)
+        return '<h1>New User has been created!</h1>'
 
-@app.route("/error", methods=["POST"])
-def error():
-    return render_template("error.html")
+    return render_template("register.html", form=form)
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    username = form.username.data
+    password = form.password.data
+    message = "Login"
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=username).first()
+        if user:
+            if user.password == password:
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
 
-"""
-@app.route("/home", methods=["POST"])
-def home(user):
-    username = request.form.get("username")
-    password = request.form.get("password")
-    user = Users.query.get(username)
-    if user is None:
-        return render_template("error.html", user=username, message="does not exist")
-    #user_exists = Users.query(username)
-    #if user_exists == username:
-    #    print(f"username: {username}")
-    #    return render_template("error.html")
-    db.session.add(user)
-    db.session.commit()
-    return render_template("home.html")
-"""
+        return '<h1>Invalid username or password!</h1>'
+
+    return render_template("login.html", form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html", name=current_user.username)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     with application.app_context():
         main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@app.route("/names")
-def names():
-    names = ["Alice", "Bob", "Combucha", "Donday"]
-    return render_template("names.html", names=names)
-
-@app.route("/hello", methods=["POST"])
-def hello():
-    name = request.form.get("name")
-    return render_template("hello.html", name=name)
--- models.py
+++ models.py
@@ -1,36 +1,45 @@
+import os
+import csv
+
+from flask import Flask, session, render_template, request, redirect, url_for
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 
-""" class Flight(db.Model):
-    __tablename__ = "flights"
-    id = db.Column(db.Integer, primary_key=True)
-    origin = db.Column(db.String, nullable=False)
-    destination = db.Column(db.String, nullable=False)
-    duration = db.Column(db.Integer, nullable=False)
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[
+                             InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('Remember me')
 
 
-class Passenger(db.Model):
-    __tablename__ = "passengers"
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[
+                             InputRequired(), Length(min=8, max=80)])
+
+
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    flight_id = db.Column(db.Integer, db.ForeignKey(
-        "flights.id"), nullable=False) """
+    username = db.Column(db.String(15), unique=True)
+    password = db.Column(db.String(255))
+
 
 class Books(db.Model):
-    _tablename__ = "books"
     id = db.Column(db.Integer, primary_key=True)
     isbn = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
     year = db.Column(db.String, nullable=False)
-
-class Users(db.Model):
-    _tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, nullable=False)
